b6bae7148a2d5a4d909f899160dc2a8d98e72342
JeanOryon <48481925+JeanOryon@users.noreply.github.com>
2023-02-23 19:43:08 -0400

    animation
    adding animation to the main page

--- js/imaginario.js
+++ js/imaginario.js
@@ -81,6 +81,21 @@ function displaymenu() {
 
 }
 
+var observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show","active");
+        } else {
+            entry.target.classList.remove("show","active")
+        }
+    });
+});
+
+var hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
 
 
 
